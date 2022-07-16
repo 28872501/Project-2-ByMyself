b5dd630f727166c3db702c97397cf2fa498c768e
2501 <873682953@qq.com>
2022-07-16 22:57:35 +0800

    创建SpringBoot-web应用

---
+++ src/test/java/com/exercise/project2bymyself/Project2ByMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.project2bymyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2ByMyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
